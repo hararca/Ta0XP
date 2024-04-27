c4ae199d04b36f51af3f37d3d472a67d33b8b738
hararca <hararca@proton.me>
2024-04-27 15:41:55 +0200

    first commit

---
+++ src/assets/wallpapers/wallpapers.js
@@ -0,0 +1,31 @@
+export const wallpapers = [
+  {
+    id: 1,
+    src: 'assets/wallpapers/lain.png',
+  },
+
+  {
+    id: 2,
+    src: 'assets/wallpapers/kdick.png',
+  },
+
+  {
+    id: 3,
+    src: 'assets/wallpapers/fondos_ta0XP_1.jpg',
+  },
+
+  {
+    id: 4,
+    src: 'assets/wallpapers/fondos_ta0XP_2.jpg',
+  },
+
+  {
+    id: 5,
+    src: 'assets/wallpapers/fondos_ta0XP_3.jpg',
+  },
+
+  {
+    id: 6,
+    src: 'assets/wallpapers/fondos_ta0XP_4.jpg',
+  },
+];
