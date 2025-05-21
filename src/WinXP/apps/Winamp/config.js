47b8ec9d2769ff0ea3a6db33fad6596d1f6720d6
hararca <hararca@proton.me>
2025-05-21 13:50:40 +0200

    Add episode 70

--- src/WinXP/apps/Winamp/config.js
+++ src/WinXP/apps/Winamp/config.js
@@ -4,8 +4,16 @@ const album = 'Every future time was better';
 
 export const initialTracks = [
   {
-    url:
-      'https://archive.org/download/ta0-71/ta0-71_fucking_shit_show.mp3',
+    url: 'https://archive.org/download/ta0-70-rachael/Ta0-70_Rachael.mp3',
+    duration: 7130.5,
+    metaData: {
+      title: 'ta-0-70-con-Rachael',
+      artist: 'Tendiendo a 0',
+      album,
+    },
+  },
+  {
+    url: 'https://archive.org/download/ta0-71/ta0-71_fucking_shit_show.mp3',
     duration: 6985.01,
     metaData: {
       title: 'ta-0-71-con-Fucking-Shit-Show',
@@ -14,8 +22,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-72-con-plnk/Ta0-72%20con%20PLNK.mp3',
+    url: 'https://archive.org/download/ta-0-72-con-plnk/Ta0-72%20con%20PLNK.mp3',
     duration: 7081.96,
     metaData: {
       title: 'ta-0-72-con-PLNK',
@@ -24,8 +31,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-73-con-gordon-blue/Ta0-73%20con%20Gordon%20Blue.mp3',
+    url: 'https://archive.org/download/ta-0-73-con-gordon-blue/Ta0-73%20con%20Gordon%20Blue.mp3',
     duration: 7055.78,
     metaData: {
       title: 'Ta0_73_Gordon_BLue',
@@ -34,8 +40,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-74-con-parautas/Ta0-74%20con%20Parautas.mp3',
+    url: 'https://archive.org/download/ta-0-74-con-parautas/Ta0-74%20con%20Parautas.mp3',
     duration: 7045.9,
     metaData: {
       title: 'Ta0_74_Parautas',
@@ -44,8 +49,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-75-con-azop/Ta0-75%20con%20Azop.mp3',
+    url: 'https://archive.org/download/ta-0-75-con-azop/Ta0-75%20con%20Azop.mp3',
     duration: 7046.24,
     metaData: {
       title: 'Ta0_75_Azop',
@@ -72,8 +76,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-78-con-torsion-jim/Ta0-78%20con%20Torsion%20Jim.mp3',
+    url: 'https://archive.org/download/ta-0-78-con-torsion-jim/Ta0-78%20con%20Torsion%20Jim.mp3',
     duration: 7131.13,
     metaData: {
       title: 'Ta0_78_Torsion_Jim',
@@ -82,8 +85,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-79-con-obleak/Ta0-79%20con%20Obleak.mp3',
+    url: 'https://archive.org/download/ta-0-79-con-obleak/Ta0-79%20con%20Obleak.mp3',
     duration: 7113.16,
     metaData: {
       title: 'Ta0_79_0bleak',
@@ -92,8 +94,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-80-con-b-10burd-3n/Ta0-80%20con%20b10burd3n.mp3',
+    url: 'https://archive.org/download/ta-0-80-con-b-10burd-3n/Ta0-80%20con%20b10burd3n.mp3',
     duration: 7185.5,
     metaData: {
       title: 'Ta0_80_b10burd3n',
@@ -102,8 +103,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-81-con-iker-ormazabal-martinez/Ta0-81%20con%20Iker%20Ormazabal%20Marti%CC%81nez.mp3',
+    url: 'https://archive.org/download/ta-0-81-con-iker-ormazabal-martinez/Ta0-81%20con%20Iker%20Ormazabal%20Marti%CC%81nez.mp3',
     duration: 7181.14,
     metaData: {
       title: 'Ta0_81_Ormazabal_Martínez',
@@ -112,8 +112,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-82-con-alemal/Ta0-82%20con%20Alemal.mp3',
+    url: 'https://archive.org/download/ta-0-82-con-alemal/Ta0-82%20con%20Alemal.mp3',
     duration: 7072.57,
     metaData: {
       title: 'Ta0_82_Alemal',
@@ -122,8 +121,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-83-con-willy-the-squid/Ta0-83%20con%20Willy%20the%20Squid.mp3',
+    url: 'https://archive.org/download/ta-0-83-con-willy-the-squid/Ta0-83%20con%20Willy%20the%20Squid.mp3',
     duration: 6914.5,
     metaData: {
       title: 'Ta0_83_Willy_the_Squid',
@@ -132,8 +130,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-84-con-yesavage-sauvage/Ta0-84%20con%20Yesavage%20Sauvage.mp3',
+    url: 'https://archive.org/download/ta-0-84-con-yesavage-sauvage/Ta0-84%20con%20Yesavage%20Sauvage.mp3',
     duration: 7182.3,
     metaData: {
       title: 'Ta0_84_Yesavage_Sauvage',
@@ -142,8 +139,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-85-con-sputnik-pussy/Ta0-85%20con%20Sputnik%20Pussy.mp3',
+    url: 'https://archive.org/download/ta-0-85-con-sputnik-pussy/Ta0-85%20con%20Sputnik%20Pussy.mp3',
     duration: 7182.52,
     metaData: {
       title: 'Ta0_85_Sputnik_Pussy',
@@ -152,8 +148,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-86-con-the-silly-tang/Ta0-86%20con%20The%20Silly%20Tang%20.mp3',
+    url: 'https://archive.org/download/ta-0-86-con-the-silly-tang/Ta0-86%20con%20The%20Silly%20Tang%20.mp3',
     duration: 6670.46,
     metaData: {
       title: 'Ta0_86_The_Silly_Tang',
@@ -162,8 +157,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-87-con-az-rotator/Ta0-87%20con%20AZ-Rotator.mp3',
+    url: 'https://archive.org/download/ta-0-87-con-az-rotator/Ta0-87%20con%20AZ-Rotator.mp3',
     duration: 6505.54,
     metaData: {
       title: 'Ta0_87_AZ_Rotator',
@@ -172,8 +166,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-88-con-neuro-habitat-extend/Ta0-88%20con%20Neuro%20Habitat%20%28extend%29.mp3',
+    url: 'https://archive.org/download/ta-0-88-con-neuro-habitat-extend/Ta0-88%20con%20Neuro%20Habitat%20%28extend%29.mp3',
     duration: 6983.88,
     metaData: {
       title: 'Ta0_88_Neuro_Habitat_extend',
@@ -182,8 +175,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-89-con-honson/Ta0-89%20con%20Honson.mp3',
+    url: 'https://archive.org/download/ta-0-89-con-honson/Ta0-89%20con%20Honson.mp3',
     duration: 7119.31,
     metaData: {
       title: 'Ta0_89_Honson',
@@ -192,8 +184,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-90-con-gordon-blue/Ta0-90%20con%20Gordon%20Blue.mp3',
+    url: 'https://archive.org/download/ta-0-90-con-gordon-blue/Ta0-90%20con%20Gordon%20Blue.mp3',
     duration: 7134.21,
     metaData: {
       title: 'Ta0_90_Gordon_Blue',
@@ -202,8 +193,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-91-con-blach/Ta0-91%20con%20Blach.mp3',
+    url: 'https://archive.org/download/ta-0-91-con-blach/Ta0-91%20con%20Blach.mp3',
     duration: 6974.55,
     metaData: {
       title: 'Ta0_91_Blach',
@@ -212,8 +202,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-92-la-vuelta-al-core/Ta0-92%20-%20La%20vuelta%20al%20Core.mp3',
+    url: 'https://archive.org/download/ta-0-92-la-vuelta-al-core/Ta0-92%20-%20La%20vuelta%20al%20Core.mp3',
     duration: 7047.51,
     metaData: {
       title: 'Ta0_92_La_Vuelta_Al_Core',
@@ -222,8 +211,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/rape-ta-0-con-leidilei-y-jean-michel-one/Ta0-93%20El%20RapeTa0%20con%20Leidilei%20y%20Jean%20Michel%20One.mp3',
+    url: 'https://archive.org/download/rape-ta-0-con-leidilei-y-jean-michel-one/Ta0-93%20El%20RapeTa0%20con%20Leidilei%20y%20Jean%20Michel%20One.mp3',
     duration: 6503.43,
     metaData: {
       title: 'Ta0_93_RapeTa0_Con_Leidilei_Y_Jean_Michel_One',
@@ -232,8 +220,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-94-con-null-drone-daemon/Ta0-94%20con%20Null%20Drone%20Daemon.mp3',
+    url: 'https://archive.org/download/ta-0-94-con-null-drone-daemon/Ta0-94%20con%20Null%20Drone%20Daemon.mp3',
     duration: 5906.58,
     metaData: {
       title: 'Ta0_94_Null_Drone_Daemon',
@@ -260,8 +247,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-97-con-neuro-habitat/Ta0-97%20con%20NeuroHabitat.mp3',
+    url: 'https://archive.org/download/ta-0-97-con-neuro-habitat/Ta0-97%20con%20NeuroHabitat.mp3',
     duration: 6310.06,
     metaData: {
       title: 'Ta0_97_Neuro_Habitat',
@@ -270,8 +256,7 @@ export const initialTracks = [
     },
   },
   {
-    url:
-      'https://archive.org/download/ta-0-98-con-manson-x/Ta0-98%20con%20Manson%20X.mp3',
+    url: 'https://archive.org/download/ta-0-98-con-manson-x/Ta0-98%20con%20Manson%20X.mp3',
     duration: 5985.76,
     metaData: {
       title: 'Ta0_98_Manson_X',
@@ -289,4 +274,3 @@ export const initialTracks = [
     },
   },
 ];
-
